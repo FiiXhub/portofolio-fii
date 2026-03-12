d7380a9a6365a30dc83da9e5c8838cd2743b545f
FiiXhub <andiabdwahabzainal@gmail.com>
2026-03-12 11:58:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,61 @@
-// visitor counter
+// SKILL CHART
 
-fetch("https://api.countapi.xyz/hit/fii-portfolio/visits")
+const ctx = document.getElementById("skillChart");
 
-.then(res => res.json())
-
-.then(data => {
-
-document.getElementById("visitor-count").innerText = data.value
-
+new Chart(ctx,{
+type:"bar",
+data:{
+labels:[
+"JavaScript",
+"Discord API",
+"AI API",
+"Web Development",
+"WhatsApp Dev",
+"Discord Dev"
+],
+datasets:[{
+label:"Skill Level",
+data:[90,85,80,75,85,90]
+}]
+}
 })
 
 
 
-// rating system
+// VISITOR COUNTER
+
+fetch("https://api.countapi.xyz/hit/fii-portfolio/visits")
+
+.then(res=>res.json())
+
+.then(data=>{
+document.getElementById("visitor-count").innerText=data.value
+})
+
+
+
+// RATING
 
 function rate(star){
 
 localStorage.setItem("rating",star)
 
-document.getElementById("rating-result").innerText =
-"Terima kasih memberi rating " + star + " ⭐"
+document.getElementById("rating-result").innerText=
+"Terima kasih memberi rating "+star+" ⭐"
 
 }
+
+
+
+// GITHUB STATS
+
+fetch("https://api.github.com/users/USERNAME")
+
+.then(res=>res.json())
+
+.then(data=>{
+
+document.getElementById("repo").innerText=data.public_repos
+document.getElementById("followers").innerText=data.followers
+
+})
